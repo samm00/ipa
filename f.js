05ac620775b459ee9a48f11358fff63474594f8f
Sam <sammiller19@roslynschools.org>
2018-11-11 14:58:10 +0000

    initial commit

---
+++ f.js
@@ -0,0 +1,525 @@
+var all=
+[
+ {
+   Symbol: "ɑ",
+   decimal: 593,
+   hex: "0251",
+   value: "open back unrounded"
+ },
+ {
+   Symbol: "ɐ",
+   decimal: 592,
+   hex: "0250",
+   value: "open-mid schwa"
+ },
+ {
+   Symbol: "ɒ",
+   decimal: 594,
+   hex: "0252",
+   value: "open back rounded"
+ },
+ {
+   Symbol: "æ",
+   decimal: 230,
+   hex: "00E6",
+   value: "raised open front unrounded"
+ },
+ {
+   Symbol: "ɓ",
+   decimal: 595,
+   hex: "0253",
+   value: "vd bilabial implosive"
+ },
+ {
+   Symbol: "ʙ",
+   decimal: 665,
+   hex: "0299",
+   value: "vd bilabial trill"
+ },
+ {
+   Symbol: "β",
+   decimal: 946,
+   hex: "03B2",
+   value: "vd bilabial fricative"
+ },
+ {
+   Symbol: "ɔ",
+   decimal: 596,
+   hex: "0254",
+   value: "open-mid back rounded"
+ },
+ {
+   Symbol: "ɕ",
+   decimal: 597,
+   hex: "0255",
+   value: "vl alveolopalatal fricative"
+ },
+ {
+   Symbol: "ç",
+   decimal: 231,
+   hex: "00E7",
+   value: "vl palatal fricative"
+ },
+ {
+   Symbol: "ɗ",
+   decimal: 599,
+   hex: "0257",
+   value: "vd alveolar implosive"
+ },
+ {
+   Symbol: "ɖ",
+   decimal: 598,
+   hex: "0256",
+   value: "vd retroflex plosive"
+ },
+ {
+   Symbol: "ð",
+   decimal: 240,
+   hex: "00F0",
+   value: "vd dental fricative"
+ },
+ {
+   Symbol: "ʤ",
+   decimal: 676,
+   hex: "02A4",
+   value: "vd postalveolar affricate"
+ },
+ {
+   Symbol: "ə",
+   decimal: 601,
+   hex: "0259",
+   value: "schwa"
+ },
+ {
+   Symbol: "ɘ",
+   decimal: 600,
+   hex: "0258",
+   value: "close-mid schwa"
+ },
+ {
+   Symbol: "ɚ",
+   decimal: 602,
+   hex: "025A",
+   value: "rhotacized schwa"
+ },
+ {
+   Symbol: "ɛ",
+   decimal: 603,
+   hex: "025B",
+   value: "open-mid front unrounded"
+ },
+ {
+   Symbol: "ɜ",
+   decimal: 604,
+   hex: "025C",
+   value: "open-mid central"
+ },
+ {
+   Symbol: "ɝ",
+   decimal: 605,
+   hex: "025D",
+   value: "rhotacized open-mid central"
+ },
+ {
+   Symbol: "ɞ",
+   decimal: 606,
+   hex: "025E",
+   value: "open-mid central rounded"
+ },
+ {
+   Symbol: "ɟ",
+   decimal: 607,
+   hex: "025F",
+   value: "vd palatal plosive"
+ },
+ {
+   Symbol: "ʄ",
+   decimal: 644,
+   hex: "0284",
+   value: "vd palatal implosive"
+ },
+ {
+   Symbol: "ɡ",
+   decimal: 609,
+   hex: "0261",
+   value: "vd velar plosive\n(but the IPA has ruled that an ordinary g is also acceptable)"
+ },
+ {
+   Symbol: "ɠ",
+   decimal: 608,
+   hex: "0260",
+   value: "vd velar implosive"
+ },
+ {
+   Symbol: "ɢ",
+   decimal: 610,
+   hex: "0262",
+   value: "vd uvular plosive"
+ },
+ {
+   Symbol: "ʛ",
+   decimal: 667,
+   hex: "029B",
+   value: "vd uvular implosive"
+ },
+ {
+   Symbol: "ɦ",
+   decimal: 614,
+   hex: "0266",
+   value: "vd glottal fricative"
+ },
+ {
+   Symbol: "ɧ",
+   decimal: 615,
+   hex: "0267",
+   value: "vl multiple-place fricative"
+ },
+ {
+   Symbol: "ħ",
+   decimal: 295,
+   hex: "0127",
+   value: "vl pharyngeal fricative"
+ },
+ {
+   Symbol: "ɥ",
+   decimal: 613,
+   hex: "0265",
+   value: "labial-palatal approximant"
+ },
+ {
+   Symbol: "ʜ",
+   decimal: 668,
+   hex: "029C",
+   value: "vl epiglottal fricative"
+ },
+ {
+   Symbol: "ɨ",
+   decimal: 616,
+   hex: "0268",
+   value: "close central unrounded"
+ },
+ {
+   Symbol: "ɪ",
+   decimal: 618,
+   hex: "026A",
+   value: "lax close front unrounded"
+ },
+ {
+   Symbol: "ʝ",
+   decimal: 669,
+   hex: "029D",
+   value: "vd palatal fricative"
+ },
+ {
+   Symbol: "ɭ",
+   decimal: 621,
+   hex: "026D",
+   value: "vd retroflex lateral"
+ },
+ {
+   Symbol: "ɬ",
+   decimal: 620,
+   hex: "026C",
+   value: "vl alveolar lateral fricative"
+ },
+ {
+   Symbol: "ɫ",
+   decimal: 619,
+   hex: "026B",
+   value: "velarized vd alveolar lateral"
+ },
+ {
+   Symbol: "ɮ",
+   decimal: 622,
+   hex: "026E",
+   value: "vd alveolar lateral fricative"
+ },
+ {
+   Symbol: "ʟ",
+   decimal: 671,
+   hex: "029F",
+   value: "vd velar lateral"
+ },
+ {
+   Symbol: "ɱ",
+   decimal: 625,
+   hex: "0271",
+   value: "vd labiodental nasal"
+ },
+ {
+   Symbol: "ɯ",
+   decimal: 623,
+   hex: "026F",
+   value: "close back unrounded"
+ },
+ {
+   Symbol: "ɰ",
+   decimal: 624,
+   hex: "0270",
+   value: "velar approximant"
+ },
+ {
+   Symbol: "ŋ",
+   decimal: 331,
+   hex: "014B",
+   value: "vd velar nasal"
+ },
+ {
+   Symbol: "ɳ",
+   decimal: 627,
+   hex: "0273",
+   value: "vd retroflex nasal"
+ },
+ {
+   Symbol: "ɲ",
+   decimal: 626,
+   hex: "0272",
+   value: "vd palatal nasal"
+ },
+ {
+   Symbol: "ɴ",
+   decimal: 628,
+   hex: "0274",
+   value: "vd uvular nasal"
+ },
+ {
+   Symbol: "ø",
+   decimal: 248,
+   hex: "00F8",
+   value: "front close-mid rounded"
+ },
+ {
+   Symbol: "ɵ",
+   decimal: 629,
+   hex: "0275",
+   value: "rounded schwa"
+ },
+ {
+   Symbol: "ɸ",
+   decimal: 632,
+   hex: "0278",
+   value: "vl bilabial fricative"
+ },
+ {
+   Symbol: "θ",
+   decimal: 952,
+   hex: "03B8",
+   value: "vl dental fricative"
+ },
+ {
+   Symbol: "œ",
+   decimal: 339,
+   hex: "0153",
+   value: "front open-mid rounded"
+ },
+ {
+   Symbol: "ɶ",
+   decimal: 630,
+   hex: "0276",
+   value: "front open rounded"
+ },
+ {
+   Symbol: "ʘ",
+   decimal: 664,
+   hex: "0298",
+   value: "bilabial click"
+ },
+ {
+   Symbol: "ɹ",
+   decimal: 633,
+   hex: "0279",
+   value: "vd (post)alveolar approximant"
+ },
+ {
+   Symbol: "ɺ",
+   decimal: 634,
+   hex: "027A",
+   value: "vd alveolar lateral flap"
+ },
+ {
+   Symbol: "ɾ",
+   decimal: 638,
+   hex: "027E",
+   value: "vd alveolar tap"
+ },
+ {
+   Symbol: "ɻ",
+   decimal: 635,
+   hex: "027B",
+   value: "vd retroflex approximant"
+ },
+ {
+   Symbol: "ʀ",
+   decimal: 640,
+   hex: "0280",
+   value: "vd uvular trill"
+ },
+ {
+   Symbol: "ʁ",
+   decimal: 641,
+   hex: "0281",
+   value: "vd uvular fricative"
+ },
+ {
+   Symbol: "ɽ",
+   decimal: 637,
+   hex: "027D",
+   value: "vd retroflex flap"
+ },
+ {
+   Symbol: "ʂ",
+   decimal: 642,
+   hex: "0282",
+   value: "vl retroflex fricative"
+ },
+ {
+   Symbol: "ʃ",
+   decimal: 643,
+   hex: "0283",
+   value: "vl postalveolar fricative"
+ },
+ {
+   Symbol: "ʈ",
+   decimal: 648,
+   hex: "0288",
+   value: "vl retroflex plosive"
+ },
+ {
+   Symbol: "ʧ",
+   decimal: 679,
+   hex: "02A7",
+   value: "vl postalveolar affricate"
+ },
+ {
+   Symbol: "ʉ",
+   decimal: 649,
+   hex: "0289",
+   value: "close central rounded"
+ },
+ {
+   Symbol: "ʊ",
+   decimal: 650,
+   hex: "028A",
+   value: "lax close back rounded"
+ },
+ {
+   Symbol: "ʋ",
+   decimal: 651,
+   hex: "028B",
+   value: "vd labiodental approximant"
+ },
+ {
+   Symbol: "ⱱ",
+   decimal: 11377,
+   hex: "2C71",
+   value: "voiced labiodental flap"
+ },
+ {
+   Symbol: "ʌ",
+   decimal: 652,
+   hex: "028C",
+   value: "open-mid back unrounded"
+ },
+ {
+   Symbol: "ɣ",
+   decimal: 611,
+   hex: "0263",
+   value: "vd velar fricative"
+ },
+ {
+   Symbol: "ɤ",
+   decimal: 612,
+   hex: "0264",
+   value: "close-mid back unrounded"
+ },
+ {
+   Symbol: "ʍ",
+   decimal: 653,
+   hex: "028D",
+   value: "vl labial-velar fricative"
+ },
+ {
+   Symbol: "χ",
+   decimal: 967,
+   hex: "03C7",
+   value: "vl uvular fricative"
+ },
+ {
+   Symbol: "ʎ",
+   decimal: 654,
+   hex: "028E",
+   value: "vd palatal lateral"
+ },
+ {
+   Symbol: "ʏ",
+   decimal: 655,
+   hex: "028F",
+   value: "lax close front rounded"
+ },
+ {
+   Symbol: "ʑ",
+   decimal: 657,
+   hex: "0291",
+   value: "vd alveolopalatal fricative"
+ },
+ {
+   Symbol: "ʐ",
+   decimal: 656,
+   hex: "0290",
+   value: "vd retroflex fricative"
+ },
+ {
+   Symbol: "ʒ",
+   decimal: 658,
+   hex: "0292",
+   value: "vd postalveolar fricative"
+ },
+ {
+   Symbol: "ʔ",
+   decimal: 660,
+   hex: "0294",
+   value: "glottal plosive"
+ },
+ {
+   Symbol: "ʡ",
+   decimal: 673,
+   hex: "02A1",
+   value: "vd epiglottal plosive"
+ },
+ {
+   Symbol: "ʕ",
+   decimal: 661,
+   hex: "0295",
+   value: "vd pharyngeal fricative"
+ },
+ {
+   Symbol: "ʢ",
+   decimal: 674,
+   hex: "02A2",
+   value: "vd epiglottal fricative"
+ },
+ {
+   Symbol: "ǀ",
+   decimal: 448,
+   hex: "01C0",
+   value: "dental click"
+ },
+ {
+   Symbol: "ǁ",
+   decimal: 449,
+   hex: "01C1",
+   value: "alveolar lateral click"
+ },
+ {
+   Symbol: "ǂ",
+   decimal: 450,
+   hex: "01C2",
+   value: "alveolar click"
+ },
+ {
+   Symbol: "ǃ",
+   decimal: 451,
+   hex: "01C3",
+   value: "retroflex click"
+ }
+];
---
+++ main.js
@@ -0,0 +1,53 @@
+var all=[{Symbol:"ɑ",decimal:593,hex:"\u0251",value:"open backunrounded"},{Symbol:"ɐ",decimal:592,hex:"\u0250",value:"open-mid schwa"},{Symbol:"ɒ",decimal:594,hex:"\u0252",value:"open back rounded"},{Symbol:"æ",decimal:230,hex:"\u00E6",value:"raised open frontunrounded"},{Symbol:"ɓ",decimal:595,hex:"\u0253",value:"vd bilabial implosive"},{Symbol:"ʙ",decimal:665,hex:"\u0299",value:"vd bilabial trill"},{Symbol:"β",decimal:946,hex:"\u03B2",value:"vd bilabial fricative"},{Symbol:"ɔ",decimal:596,hex:"\u0254",value:"open-mid back rounded"},{Symbol:"ɕ",decimal:597,hex:"\u0255",value:"vl alveolopalatal fricative"},{Symbol:"ç",decimal:231,hex:"\u00E7",value:"vl palatal fricative"},{Symbol:"ɗ",decimal:599,hex:"\u0257",value:"vd alveolar implosive"},{Symbol:"ɖ",decimal:598,hex:"\u0256",value:"vd retroflex plosive"},{Symbol:"ð",decimal:240,hex:"\u00F0",value:"vd dental fricative"},{Symbol:"ʤ",decimal:676,hex:"\u02A4",value:"vd postalveolar affricate"},{Symbol:"ə",decimal:601,hex:"\u0259",value:"schwa"},{Symbol:"ɘ",decimal:600,hex:"\u0258",value:"close-mid schwa"},{Symbol:"ɚ",decimal:602,hex:"\u025A",value:"rhotacized schwa"},{Symbol:"ɛ",decimal:603,hex:"\u025B",value:"open-mid frontunrounded"},{Symbol:"ɜ",decimal:604,hex:"\u025C",value:"open-mid central"},{Symbol:"ɝ",decimal:605,hex:"\u025D",value:"rhotacized open-mid central"},{Symbol:"ɞ",decimal:606,hex:"\u025E",value:"open-mid central rounded"},{Symbol:"ɟ",decimal:607,hex:"\u025F",value:"vd palatal plosive"},{Symbol:"ʄ",decimal:644,hex:"\u0284",value:"vd palatal implosive"},{Symbol:"ɡ",decimal:609,hex:"\u0261",value:"vd velar plosive\n(but the IPA has ruled that an ordinary g is also acceptable)"},{Symbol:"ɠ",decimal:608,hex:"\u0260",value:"vd velar implosive"},{Symbol:"ɢ",decimal:610,hex:"\u0262",value:"vduvular plosive"},{Symbol:"ʛ",decimal:667,hex:"\u029B",value:"vduvular implosive"},{Symbol:"ɦ",decimal:614,hex:"\u0266",value:"vd glottal fricative"},{Symbol:"ɧ",decimal:615,hex:"\u0267",value:"vl multiple-place fricative"},{Symbol:"ħ",decimal:295,hex:"\u0127",value:"vl pharyngeal fricative"},{Symbol:"ɥ",decimal:613,hex:"\u0265",value:"labial-palatal approximant"},{Symbol:"ʜ",decimal:668,hex:"\u029C",value:"vl epiglottal fricative"},{Symbol:"ɨ",decimal:616,hex:"\u0268",value:"close centralunrounded"},{Symbol:"ɪ",decimal:618,hex:"\u026A",value:"lax close frontunrounded"},{Symbol:"ʝ",decimal:669,hex:"\u029D",value:"vd palatal fricative"},{Symbol:"ɭ",decimal:621,hex:"\u026D",value:"vd retroflex lateral"},{Symbol:"ɬ",decimal:620,hex:"\u026C",value:"vl alveolar lateral fricative"},{Symbol:"ɫ",decimal:619,hex:"\u026B",value:"velarized vd alveolar lateral"},{Symbol:"ɮ",decimal:622,hex:"\u026E",value:"vd alveolar lateral fricative"},{Symbol:"ʟ",decimal:671,hex:"\u029F",value:"vd velar lateral"},{Symbol:"ɱ",decimal:625,hex:"\u0271",value:"vd labiodental nasal"},{Symbol:"ɯ",decimal:623,hex:"\u026F",value:"close backunrounded"},{Symbol:"ɰ",decimal:624,hex:"\u0270",value:"velar approximant"},{Symbol:"ŋ",decimal:331,hex:"\u014B",value:"vd velar nasal"},{Symbol:"ɳ",decimal:627,hex:"\u0273",value:"vd retroflex nasal"},{Symbol:"ɲ",decimal:626,hex:"\u0272",value:"vd palatal nasal"},{Symbol:"ɴ",decimal:628,hex:"\u0274",value:"vduvular nasal"},{Symbol:"ø",decimal:248,hex:"\u00F8",value:"front close-mid rounded"},{Symbol:"ɵ",decimal:629,hex:"\u0275",value:"rounded schwa"},{Symbol:"ɸ",decimal:632,hex:"\u0278",value:"vl bilabial fricative"},{Symbol:"θ",decimal:952,hex:"\u03B8",value:"vl dental fricative"},{Symbol:"œ",decimal:339,hex:"\u0153",value:"front open-mid rounded"},{Symbol:"ɶ",decimal:630,hex:"\u0276",value:"front open rounded"},{Symbol:"ʘ",decimal:664,hex:"\u0298",value:"bilabial click"},{Symbol:"ɹ",decimal:633,hex:"\u0279",value:"vd (post)alveolar approximant"},{Symbol:"ɺ",decimal:634,hex:"\u027A",value:"vd alveolar lateral flap"},{Symbol:"ɾ",decimal:638,hex:"\u027E",value:"vd alveolar tap"},{Symbol:"ɻ",decimal:635,hex:"\u027B",value:"vd retroflex approximant"},{Symbol:"ʀ",decimal:640,hex:"\u0280",value:"vduvular trill"},{Symbol:"ʁ",decimal:641,hex:"\u0281",value:"vduvular fricative"},{Symbol:"ɽ",decimal:637,hex:"\u027D",value:"vd retroflex flap"},{Symbol:"ʂ",decimal:642,hex:"\u0282",value:"vl retroflex fricative"},{Symbol:"ʃ",decimal:643,hex:"\u0283",value:"vl postalveolar fricative"},{Symbol:"ʈ",decimal:648,hex:"\u0288",value:"vl retroflex plosive"},{Symbol:"ʧ",decimal:679,hex:"\u02A7",value:"vl postalveolar affricate"},{Symbol:"ʉ",decimal:649,hex:"\u0289",value:"close central rounded"},{Symbol:"ʊ",decimal:650,hex:"\u028A",value:"lax close back rounded"},{Symbol:"ʋ",decimal:651,hex:"\u028B",value:"vd labiodental approximant"},{Symbol:"ⱱ",decimal:11377,hex:"\u2C71",value:"voiced labiodental flap"},{Symbol:"ʌ",decimal:652,hex:"\u028C",value:"open-mid backunrounded"},{Symbol:"ɣ",decimal:611,hex:"\u0263",value:"vd velar fricative"},{Symbol:"ɤ",decimal:612,hex:"\u0264",value:"close-mid backunrounded"},{Symbol:"ʍ",decimal:653,hex:"\u028D",value:"vl labial-velar fricative"},{Symbol:"χ",decimal:967,hex:"\u03C7",value:"vluvular fricative"},{Symbol:"ʎ",decimal:654,hex:"\u028E",value:"vd palatal lateral"},{Symbol:"ʏ",decimal:655,hex:"\u028F",value:"lax close front rounded"},{Symbol:"ʑ",decimal:657,hex:"\u0291",value:"vd alveolopalatal fricative"},{Symbol:"ʐ",decimal:656,hex:"\u0290",value:"vd retroflex fricative"},{Symbol:"ʒ",decimal:658,hex:"\u0292",value:"vd postalveolar fricative"},{Symbol:"ʔ",decimal:660,hex:"\u0294",value:"glottal plosive"},{Symbol:"ʡ",decimal:673,hex:"\u02A1",value:"vd epiglottal plosive"},{Symbol:"ʕ",decimal:661,hex:"\u0295",value:"vd pharyngeal fricative"},{Symbol:"ʢ",decimal:674,hex:"\u02A2",value:"vd epiglottal fricative"},{Symbol:"ǀ",decimal:448,hex:"\u01C0",value:"dental click"},{Symbol:"ǁ",decimal:449,hex:"\u01C1",value:"alveolar lateral click"},{Symbol:"ǂ",decimal:450,hex:"\u01C2",value:"alveolar click"},{Symbol:"ǃ",decimal:451,hex:"\u01C3",value:"retroflex click"}];
+
+var all2=[{Symbol:"ˈ",decimal:712,hex:"\u02C8",value:"(primary) stress mark"},{Symbol:"ˌ",decimal:716,hex:"\u02CC",value:"secondary stress"},{Symbol:"ː",decimal:720,hex:"\u02D0",value:"length mark NB"},{Symbol:"ˑ",decimal:721,hex:"\u02D1",value:"half-length"},{Symbol:"ʼ",decimal:700,hex:"\u02BC",value:"ejective"},{Symbol:"ʴ",decimal:692,hex:"\u02B4",value:"rhotacized"},{Symbol:"ʰ",decimal:688,hex:"\u02B0",value:"aspirated"},{Symbol:"ʱ",decimal:689,hex:"\u02B1",value:"breathy-voice-aspirated"},{Symbol:"ʲ",decimal:690,hex:"\u02B2",value:"palatalized"},{Symbol:"ʷ",decimal:695,hex:"\u02B7",value:"labialized"},{Symbol:"ˠ",decimal:736,hex:"\u02E0",value:"velarized"},{Symbol:"ˤ",decimal:740,hex:"\u02E4",value:"pharyngealized"},{Symbol:"˞",decimal:734,hex:"\u02DE",value:"rhotacized"},{Symbol:"n̥ d̥",decimal:805,hex:"\u0325",value:"voiceless"},{Symbol:"ŋ̊",decimal:778,hex:"\u030A",value:"voiceless (use if character has descender)"},{Symbol:"b̤ a̤",decimal:804,hex:"\u0324",value:"breathy voiced"},{Symbol:"t̪ d̪",decimal:810,hex:"\u032A",value:"dental"},{Symbol:"s̬ t̬",decimal:812,hex:"\u032C",value:"voiced"},{Symbol:"b̰ a̰",decimal:816,hex:"\u0330",value:"creaky voiced"},{Symbol:"t̺ d̺",decimal:826,hex:"\u033A",value:"apical"},{Symbol:"t̼ d̼",decimal:828,hex:"\u033C",value:"linguolabial"},{Symbol:"t̻ d̻",decimal:827,hex:"\u033B",value:"laminal"},{Symbol:"t̚",decimal:794,hex:"\u031A",value:"not audibly released"},{Symbol:"ɔ̹",decimal:825,hex:"\u0339",value:"more rounded"},{Symbol:"ẽ",decimal:771,hex:"\u0303",value:"nasalized"},{Symbol:"ɔ̜",decimal:796,hex:"\u031C",value:"less rounded"},{Symbol:"u̟",decimal:799,hex:"\u031F",value:"advanced"},{Symbol:"e̠",decimal:800,hex:"\u0320",value:"retracted"},{Symbol:"ë",decimal:776,hex:"\u0308",value:"centralized"},{Symbol:"l̴ n̴",decimal:820,hex:"\u0334",value:"velarized or pharyngealized"},{Symbol:"ɫ",decimal:619,hex:"\u026B",value:"(ready-made combination, dark l)"},{Symbol:"e̽",decimal:829,hex:"\u033D",value:"mid-centralized"},{Symbol:"e̝ ɹ̝",decimal:797,hex:"\u031D",value:"raised"},{Symbol:"m̩ n̩ l̩",decimal:809,hex:"\u0329",value:"syllabic"},{Symbol:"e̞ β̞",decimal:798,hex:"\u031E",value:"lowered"},{Symbol:"e̯",decimal:815,hex:"\u032F",value:"non-syllabic"},{Symbol:"e̘",decimal:792,hex:"\u0318",value:"advanced tongue root"},{Symbol:"e̙",decimal:793,hex:"\u0319",value:"retracted tongue root"},{Symbol:"ĕ",decimal:774,hex:"\u0306",value:"extra-short"},{Symbol:"e̋",decimal:779,hex:"\u030B",value:"extra high tone"},{Symbol:"é",decimal:769,hex:"\u0301",value:"high tone"},{Symbol:"ē",decimal:772,hex:"\u0304",value:"mid tone"},{Symbol:"è",decimal:768,hex:"\u0300",value:"low tone"},{Symbol:"ȅ",decimal:783,hex:"\u030F",value:"extra low tone"},{Symbol:"x͜x",decimal:860,hex:"\u035C",value:"tie bar below"},{Symbol:"x͡x",decimal:865,hex:"\u0361",value:"tie bar above"},{Symbol:"↓",decimal:8595,hex:"\u2193",value:"downstep"},{Symbol:"↑",decimal:8593,hex:"\u2191",value:"upstep"},{Symbol:"→",decimal:8594,hex:"\u2192",value:"(becomes, is realized as — not recognized by the IPA)"},{Symbol:"↗",decimal:8599,hex:"\u2197",value:"global rise"},{Symbol:"↘",decimal:8600,hex:"\u2198",value:"global fall"}];
+
+var hoverT=document.getElementById("hoverText");
+
+var slider0=document.getElementById("slider0");
+
+function change(){
+    for(var i=0;i<all.length;i++){
+        if(slider0.checked&&i<all2.length)
+            document.getElementById("let"+i).innerHTML=all2[i]["hex"]+"  ";
+        else if(slider0.checked)
+            document.getElementById("let"+i).innerHTML="";
+        else
+            document.getElementById("let"+i).innerHTML=all[i]["hex"]+"  ";
+    }
+}
+
+function writes(i){
+    if(slider0.checked)
+        document.getElementById("typing").value+=all2[i]["hex"];
+    else
+        document.getElementById("typing").value+=all[i]["hex"];
+}
+
+function over(i){
+    if(slider0.checked)
+        hoverT.innerHTML=all2[i]["value"];
+    else
+        hoverT.innerHTML=all[i]["value"];
+    hoverT.style="background-color: #ccc";
+}
+
+function out(){
+    document.getElementById("hoverText").innerHTML="";
+    document.getElementById("hoverText").style="background-color: #fff";
+}
+
+var typ=document.getElementById("typing");
+
+setInterval(function(){
+    typ.focus();
+});
+
+function clears(){
+    typ.value="";
+}
+
+function copys(){
+    typ.select();
+    document.execCommand('copy');
+}
